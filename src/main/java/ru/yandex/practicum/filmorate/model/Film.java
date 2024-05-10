cf5cd2949ac5e1b42a65dfafaa4f2e88074e8da5
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-05-10 18:46:16 +0500

    Checkstyle: исправить ошибки синтаксиса 3

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 
 /**
  * Film.
